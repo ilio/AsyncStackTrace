bcd6c11a079145ee6ad15c05cd9e565347c5c098
Igor <silent.up@outlook.com>
2015-09-27 07:42:05 +0300

    add nuget

--- AsyncStackTrace45/Properties/AssemblyInfo.cs
+++ AsyncStackTrace45/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AsyncStackTrace45")]
+[assembly: AssemblyTitle("AsyncStackTrace")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AsyncStackTrace45")]
+[assembly: AssemblyProduct("AsyncStackTrace")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
